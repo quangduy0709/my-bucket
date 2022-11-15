10056df6ab38233c05a0d3022673eb4738f07e87
quangduy0709 <quangduy0709@gmail.com>
2022-11-15 17:24:45 +0700

    fix: ....

--- test.ps1
+++ test.ps1
@@ -2,3 +2,4 @@ $WshShell = New-Object -comObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut("$Home\Desktop\Kompad.lnk")
 $Shortcut.TargetPath = "C:\Users\$env:UserName\scoop\apps\kompad\3.0\PFiles\kompad\kompad.exe"
 $Shortcut.Save()
+
