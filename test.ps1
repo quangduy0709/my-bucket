811bfd1c40c4c82d2393dd717d0c600b3a104c1b
quangduy0709 <quangduy0709@gmail.com>
2022-11-16 11:31:24 +0700

    a

--- test.ps1
+++ test.ps1
@@ -1,1 +1,1 @@
-Move-Item -Path C:\Users\quang\scoop\apps\hello\current\PFiles\kompad\kompad.exe -Destination C:\Users\quang\scoop\apps\hello\current
+Copy-Item -Path C:\Users\quang\scoop\apps\hello\current\PFiles\kompad\kompad.exe -Destination C:\Users\quang\scoop\apps\hello\current
